c855db5988b57e08eb354be94c699787a7d5f8f3
Arnold <antaloczi.arnold@gmail.com>
2021-08-09 14:43:33 +0200

    1. task done

--- backend/src/models/service.model.js
+++ backend/src/models/service.model.js
@@ -1,1 +1,34 @@
 // 1. Feladat: pótold a service modellt.
+const mongoose = require('mongoose');
+
+const ServiceSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  since: {
+    type: Number,
+    required: true,
+  },
+  street: {
+    type: String,
+    required: false
+  },
+  city: {
+    type: String,
+    required: false
+  },
+  country: {
+    type: String,
+    required: false
+  },
+  active: {
+    type: Boolean,
+    default: true,
+    required: false
+  }
+}, {
+  timeStamps: true
+});
+
+module.exports = mongoose.model('service', ServiceSchema);
